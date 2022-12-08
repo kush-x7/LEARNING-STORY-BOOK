93bb71291e6c8da7f6f164c5c9f83bd7843b8fbf
kushagra <kushagratripathi7@gmail.com>
2022-12-08 18:03:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PrimaryButton from "./components/Button";
+
+function App() {
+  return (
+    <>
+      <PrimaryButton>Click me</PrimaryButton>
+    </>
+  );
+}
+
+export default App;
